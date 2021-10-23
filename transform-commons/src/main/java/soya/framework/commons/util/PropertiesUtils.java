0867076b72d480b2295f448232b286059e781d3a
Qun Wen <43383172+soyastudio@users.noreply.github.com>
2021-10-23 07:50:28 -0700

    Schema

---
+++ transform-commons/src/main/java/soya/framework/commons/util/PropertiesUtils.java
@@ -0,0 +1,14 @@
+package soya.framework.commons.util;
+
+import java.util.Properties;
+
+public class PropertiesUtils {
+    private PropertiesUtils() {
+    }
+
+    public static Properties compile(Properties properties) {
+        Properties result = new Properties();
+
+        return result;
+    }
+}
